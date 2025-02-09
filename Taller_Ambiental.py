c42014963c8010889a2554eb1870f6af76384551
Carlos Boyacen <boyacen27@gmail.com>
2025-02-09 05:37:26 -0500

    Uploading files for the first time

---
+++ Taller_Ambiental.py
@@ -0,0 +1,5 @@
+############## TALLER PROBLEMÁTICA AMBIENTAL ##################
+import pandas as pd
+df = pd.read_csv("C:/Users/boyac/OneDrive/Desktop/Python/Clases_Personalizadas/Taller_GitHub/Bdatos.csv")
+Des = pd.read_excel("C:/Users/boyac/OneDrive/Desktop/Python/Clases_Personalizadas/Taller_GitHub/Descripcion.xlsx")
+
